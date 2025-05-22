5ecabc2e76aab4d54919801169fa6b6b51fb2d2d
Jarek Lin <s410673074@gmail.com>
2025-05-22 14:20:59 +0800

    feat: typos-lsp & avante config

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -91,6 +91,21 @@ lspconfig.graphql.setup {
     debounce_text_changes = 150,
   },
 }
+
+lspconfig.typos_lsp.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  -- Logging level of the language server. Logs appear in :LspLog. Defaults to error.
+  cmd_env = { RUST_LOG = "error" },
+  init_options = {
+    -- Custom config. Used together with a config file found in the workspace or its parents,
+    -- taking precedence for settings declared in both.
+    -- Equivalent to the typos `--config` cli argument.
+    -- How typos are rendered in the editor, can be one of an Error, Warning, Info or Hint.
+    -- Defaults to error.
+    diagnosticSeverity = "Error",
+  },
+}
 -- rust
 -- lspconfig.rust_analyzer.setup {
 --   on_attach = on_attach,
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -140,19 +140,18 @@ local plugins = {
     lazy = false,
     version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
     opts = {
+      mode = "agentic",
       -- add any opts here
-      -- provider = "openai",
+      provider = "claude",
       -- auto_suggestions_provider = "openai",
       -- -- auto_suggestions_provider = "openai", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
-      -- openai = {
-      --   endpoint = "https://api.deepseek.com/v1",
-      --   model = "deepseek-chat",
-      --   timeout = 30000, -- Timeout in milliseconds
-      --   temperature = 0,
-      max_tokens = 10000,
-      --   -- optional
-      --   -- api_key_name = "", -- default OPENAI_API_KEY if not set
-      -- },
+      claude = {
+        endpoint = "https://api.anthropic.com",
+        model = "claude-3-5-sonnet-20241022",
+        temperature = 0,
+        max_tokens = 4096,
+        disable_tools = true
+      },
       hints = { enabled = false },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`

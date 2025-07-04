f789d21d989ccf215bd6e9beb5fc3ea7dbc88dfd
Jarek Lin <s410673074@gmail.com>
2025-07-04 13:16:14 +0800

    feat: [nvimtree] fix updting currnet working dir when opening external file

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -7,12 +7,16 @@ local options = {
   hijack_netrw = true,
   hijack_cursor = true,
   hijack_unnamed_buffer_when_opening = false,
-  sync_root_with_cwd = true,
-  respect_buf_cwd = true,
+
+  -- Keep directory sync but prevent external root changes
+  sync_root_with_cwd = true, -- Allow sync for other plugins
+  respect_buf_cwd = true, -- Respect buffer directory changes
   update_focused_file = {
     enable = true,
-    update_cwd = true,
+    update_cwd = true, -- Allow CWD updates for plugin compatibility
+    update_root = false, -- Prevent root changes when going to external files
   },
+
   view = {
     adaptive_size = false,
     side = "left",
@@ -29,17 +33,30 @@ local options = {
   actions = {
     open_file = {
       resize_window = true,
+      -- Add this to prevent working directory changes when opening files
+      window_picker = {
+        enable = true,
+        picker = "default",
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+        exclude = {
+          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+          buftype = { "nofile", "terminal", "help" },
+        },
+      },
+    },
+    change_dir = {
+      enable = true, -- Allow manual directory changes
+      global = false, -- Don't change global working directory
+      restrict_above_cwd = true, -- Restrict changes to stay within project
     },
   },
   renderer = {
     root_folder_label = false,
     highlight_git = false,
     highlight_opened_files = "none",
-
     indent_markers = {
       enable = false,
     },
-
     icons = {
       show = {
         file = true,
@@ -47,7 +64,6 @@ local options = {
         folder_arrow = true,
         git = false,
       },
-
       glyphs = {
         default = "󰈚",
         symlink = "",
